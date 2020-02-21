f641b94a85d3b9a53bd2874f603ef5995740dcae
Alberto Plata <albertoplata@pop-os.localdomain>
2020-02-20 22:21:28 -0600

    Lab04

---
+++ Laboratorios/lab04/funciones.js
@@ -0,0 +1,65 @@
+
+fun4();
+
+function fun1(){
+  const rows = prompt("Ingresa un número");
+  var j=1;
+  var output = "<table class='table table-dark table-hover'><tr><th>n</th><th>n^2</th><th>n^3</th></tr>"
+  for(i=1;i<=rows;i++){
+    output = output + "<tr>";
+    while(j<=3){
+      output = output + "<td>" + i*j + "</td>";
+      j = j+1;
+    }
+    output = output + "</tr>";
+    j = 1;
+  }
+  output = output + "</table>";
+  document.write(output);
+}
+
+function fun2 (){
+  const currentTime = Date.now()
+  const num1 = Math.floor(Math.random() * 10)
+  const num2 = Math.floor(Math.random() * 10)
+  const numSum = prompt("Cuánto es " + num1.toString() + " + " + num2.toString() + "?")
+  if(numSum == (num1 + num2)){
+    window.alert("Correcto\nTiempo de respuesta: " + (Date.now()-currentTime)/1000 + " s");
+  }else{
+    window.alert("Incorrecto\nTiempo de respuesta: " + (Date.now()-currentTime)/1000 + " s");
+  }
+}
+
+function fun3 (){
+  var positive = 0
+  var zeros = 0
+  var negative = 0
+  const arr = prompt("Ingresa tus números separados por una coma").split(",")
+  for(i=1;i<=arr.length;i++){
+    if(arr[i] < 0){
+      negative = negative + 1
+    }else if (arr[i] == 0){
+      zeros = zeros + 1
+    } else {
+      positive = positive + 1
+    }
+  }
+  window.alert("Numeros positivos: " + positive + "\n" + "Numeros negativos: " + negative + "\n" + "Ceros: " + zeros);
+}
+
+function fun4(){
+  const arr = prompt("Ingresa un arreglo de arreglo de números, separa los números con un espacio y los arreglos con un ;").split(";")
+  var promedios
+  var cont = 0
+  for(i=0;i<arr.length;i++){
+    var aux = arr[i].split(" ") 
+    for(j=0;j<aux.length;j++){
+      cont = cont + aux[j]
+    }
+    cont = cont / aux.length
+    promedios.push(cont)
+    cont = 0
+  }
+  window.alert("Los promedios respectivos fueron: " + promedios.toString());
+}
+
