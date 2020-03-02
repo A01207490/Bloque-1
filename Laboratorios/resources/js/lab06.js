9612599d50242d38c998cd7987a91fc082f89926
Alberto Plata <azelot65@gmail.com>
2020-03-01 19:51:52 -0600

    Drag and drop

--- Laboratorios/resources/js/lab06.js
+++ Laboratorios/resources/js/lab06.js
@@ -9,3 +9,69 @@ function normal(x) {
 	x.innerHTML = "Glide the mouse over!"
 	x.classList.remove("spooky"); 
 }
+
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip();
+});
+
+
+var wordBank = ["zelda", "metroid", "mario", "saitama", "gohan", "samus", "link", "goku", "genos", "noct", "luna", "peach", "bang"];
+var word = document.getElementById("word");
+var enterWord = document.getElementById("enterWord");
+
+word.innerHTML = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+
+enterWord.onkeyup = function(){
+  console.log(word.textContent);
+  console.log(enterWord.value);
+  console.log(word.textContent == enterWord.value);
+  if(word.textContent == enterWord.value){
+    word.innerHTML = wordBank[Math.floor(Math.random() * wordBank.length)];
+    enterWord.value= "";
+    enterWord.innerHTML= "";
+    c = 5;
+  }
+}
+
+
+var t = setInterval(showTime, 1000);
+var w = setInterval(changeWord, 5000);
+var c = 5;
+
+function showTime() {
+	document.getElementById("demo").innerHTML = c;
+	c--;
+}
+
+function changeWord() {
+	c = 5;
+	word.innerHTML = wordBank[Math.floor(Math.random() * wordBank.length)];
+    enterWord.value= "";
+    enterWord.innerHTML= "";
+}
+
+
+
+
+
+
+
+
+function allowDrop(ev, e) {
+	if (e.id == "div1") {
+		  ev.preventDefault();
+	}
+
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("text");
+ 	ev.target.appendChild(document.getElementById(data));
+ 
+}
