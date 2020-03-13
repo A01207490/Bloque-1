5d0a04d9598992785fd2528a93dd820848ea8e35
Alberto Plata <azelot65@gmail.com>
2020-03-13 15:23:11 -0600

    Avance lab 10

--- Laboratorios/choosegames.php
+++ Laboratorios/choosegames.php
@@ -1,16 +1,37 @@
 <?php
+require_once("util.php");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	if (isset($_POST["nombre"])){
+	if (isset($_POST["name"])){
+		$name = htmlspecialchars($_POST["name"]);
+		$company = htmlspecialchars($_POST["company"]);
+		
+		if (isset($_POST["type1"])){
+			$type1 = htmlspecialchars($_POST["type1"]);
+		}else{
+			$type1 = NULL;
+		}
+		if (isset($_POST["type2"])){
+			$type2 = htmlspecialchars($_POST["type2"]);
+		}else{
+			$type2 = NULL;
+		}
+		
+		include '_head.html';
+		include '_sidebar.html';_
+
+		var_dump($_POST);
+		
+		match($name, $company, $type1, $type2);
+
+		include '_footer.html';
+
+	}else{
 		die();
 	}
-	
-	
+
 } else {
 	exit('Invalid Request');
 }
 
-
-
-echo "hello";
 ?>
---
+++ Laboratorios/util.php
@@ -0,0 +1,25 @@
+<?php 
+
+function match($name, $company, $type1, $type2) {
+	if($company == "nintendo"){
+		$openworld = "resources/images/breatheofthewild.jpg";
+		$openworldtext = "The Legend of Zelda Breathe of the Wild";
+		$hackandslash = "resources/images/astralchain.jpg";
+		$hackandslashtext = "Astral Chain";
+	}else{
+		$openworld = "resources/images/horizonzerodawn.jpg";
+		$openworldtext = "Horizon Zero Dawn";
+		$hackandslash = "resources/images/devilmaycry5.jpg";
+		$hackandslashtext = "Devil May Cry 5";
+	}
+
+	if($type1 == "openworld" && $type2 == "hackandslash"){	
+		include("_match.html");
+		include("_openworld");
+		include("_hackandslash");
+	}
+
+}
+
+
+?> 
